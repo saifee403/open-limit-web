b1b4121bb5065ee1b8b35e75f8a2f8380baa6a89
saifee403 <muh.saif2001@gmail.com>
2023-08-10 18:49:05 +0500

    search bar change

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -19,7 +19,6 @@ import Logo from "../../Asssets/logo2.png";
 import { Link, useNavigate } from "react-router-dom";
 import HomeMenu from "./HomeMenu";
 import SideBar from "./Sidebar";
-import SearchBar from './SearchBar'
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { logout } from "../../redux/authReducer/action";
@@ -88,13 +87,7 @@ const Navbar = () => {
         >
           <HomeMenu />
         </Box>
-        <Box
-          minWidth={"30%"}
-          width="90%"
-          display={{ base: "none", lg: "block" }}
-        >
-          <SearchBar />
-        </Box>
+
 
         <Flex gap={{ base: "0.5rem", md: "1.5rem" }} align="center">
           <Popover>
